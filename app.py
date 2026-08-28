e8e037d1723589da2b44279fa4aab449b3f3f70c
kiran010692 <kiran010692@gmail.com>
2026-08-28 18:24:22 +0530

    code updated

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ def calculate_discount(price, discount):
     if discount < 0 or discount > 100:
         raise ValueError("Invalid discount percentage")
     return price * (1 - discount / 100)
-if __name__ == "__main__":
+
+
+if _name_ == "_main_":
     print(f"Final Price: {calculate_discount(100, 20)}")
--- test_app.py
+++ test_app.py
@@ -1,8 +1,12 @@
 import pytest
 from app import calculate_discount
+
+
 def test_calculate_discount():
     assert calculate_discount(100, 20) == 80.0
     assert calculate_discount(50, 10) == 45.0
+
+
 def test_invalid_discount():
     with pytest.raises(ValueError):
         calculate_discount(100, 150)
